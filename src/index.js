03b9e23ad2ef4acf9e63a18791da48031393dc09
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2020-09-03 19:50:00 +0700

    Update Tampilan Semua Pemain

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import "regenerator-runtime";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import main from './script/view/main.js';
-document.addEventListener("DOMContentLoaded", main);
-// import "./styles/main.css";
-import main from './scripts/view/main.js'
+import main from './scripts/main.js'
 
-// import main from './script/view/main.js';
+main();
---
+++ src/scripts/main.js
@@ -0,0 +1,80 @@
+function main()
+{
+    const baseUrl               =   "https://www.thesportsdb.com";
+
+    const getPlayer             =   () => 
+                                    {
+                                        const xhr                       =   new XMLHttpRequest();
+
+                                        xhr.onload                      =   function()
+                                                                            {
+                                                                                const responseJson          =   JSON.parse(this.responseText);
+
+                                                                                 renderAllPlayer(responseJson.player)
+                                                                            }
+
+                                        xhr.onerror                     =   function()
+                                                                            {
+                                                                                showResponseMessage();
+                                                                            }      
+                                                                    
+                                        xhr.open("GET", `${baseUrl}/api/v1/json/1/searchplayers.php?p`);
+                                        
+                                        xhr.send();
+                                    }
+
+    const renderAllPlayer       =   (players) =>
+                                    {
+                                        const listPlayerElement         =   document.querySelector("#listPlayer");
+                                        listPlayerElement.innerHTML     =   "";
+
+                                        players.forEach(player =>
+                                                        {
+                                                            listPlayerElement.innerHTML   +=  
+                                                            `
+                                                            <div class="col mb-4">
+                                                                <div class="card h-100">
+                                                                    <img src="${player.strCutout}" class="card-img-top" alt="...">
+                                                                    <div class="card-body">
+                                                                        <h5 class="card-title">${player.strPlayer}</h5>
+                                                                        <p class="card-text">${player.strDescriptionEN}</p>
+                                                                        <table class="table">
+                                                                            <tr>
+                                                                                <th>Name</th>
+                                                                                <td>${player.strPlayer}</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <th>Date Of Birth</th>
+                                                                                <td>${player.dateBorn}</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <th>Club Team</th>
+                                                                                <td>${player.strTeam}</td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <th>Nationality</th>
+                                                                                <td>${player.strNationality}</td>
+                                                                            </tr>
+
+                                                                        </table>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            `
+                                                        })
+
+
+                                    }                  
+                                    
+    const showResponseMessage   =   (message    =   "Check your Internet Connection") =>
+                                    {
+                                        alert(message);
+                                    }           
+                                    
+    document.addEventListener("DOMContentLoaded", () => 
+                                                    {
+                                                        getPlayer();
+                                                    })                                    
+}
+
+export default main();
